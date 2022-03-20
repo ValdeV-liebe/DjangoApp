ae69983685c080748135949b3285cb66bde764e9
ValdeV-liebe <valdexliebe@gmail.com>
2022-03-20 18:22:57 +0100

    modif settings.py

--- disquaire/settings.py
+++ disquaire/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = base-dir / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 INTERNAL_IPS = ['127.0.0.1']
 
